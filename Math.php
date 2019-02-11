8c6576e9b23f9f7b46587ecc45a2b17a8c9d7854
Julianna Cole <jec56@njit.edu>
2019-02-11 16:41:45 -0500

    Testing Math file

--- Math.php
+++ Math.php
@@ -5,3 +5,21 @@
  * Date: 2/11/2019
  * Time: 2:52 PM
  */
+declare (strict_types=1);
+
+final class Math
+{
+    private $ten;
+
+    private function __construct(int $ten)
+    {
+        $this->ten = $ten;
+    }
+
+    public static function EqualsTen(int $ten): self
+    {
+        return new self($ten);
+    }
+
+
+}
