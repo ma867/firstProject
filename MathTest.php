afef74bebf743cc67dc05807bcd8a2345034e51d
ma867 <ma867@njit.edu>
2019-02-11 16:41:02 -0500

    testing test file for math.php

--- MathTest.php
+++ MathTest.php
@@ -5,3 +5,20 @@
  * Date: 2/11/2019
  * Time: 2:52 PM
  */
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class MathTest extends TestCase
+{
+    public function testTenIsTen(): void
+    {
+        $this->assertEquals(10, Math::EqualsTen(10));
+    }
+    public function testTenIsNotTen(): void
+    {
+        $this->assertEquals(10, Math::EqualsTen(11));
+    }
+
+
+}
